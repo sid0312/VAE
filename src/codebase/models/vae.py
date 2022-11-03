1bfbd8646f741e3d770a1105a5c8ded15a92c95e
Siddhant Baldota <siddhantchemistry@gmail.com>
2022-11-02 19:45:45 -0700

    Update vae.py

--- src/codebase/models/vae.py
+++ src/codebase/models/vae.py
@@ -45,16 +45,27 @@ class VAE(nn.Module):
         ################################################################################
         # End of code modification
         ################################################################################
-        m,v = self.enc.encode(x)
+        m, v = self.enc.encode(x)
         z = ut.sample_gaussian(m, v)
-        s = torch.exp(self.z_prior_m)
-        P_x_given_z = torch.distributions.Normal(m, s)
-        log_P_x_given_z = P_x_given_z.log_prob(z)
+        
 
-        rec = log_P_x_given_z.sum(dim=1)
-       
+        x_hat = self.dec.decode(z)
+        rec = torch.distributions.Normal(x_hat, self.z_prior_v).log_prob(x)
         kl = ut.kl_normal(m, v, self.z_prior_m, self.z_prior_v)
         nelbo = kl + rec
+        
+        
+        
+#         m,v = self.enc.encode(x)
+#         z = ut.sample_gaussian(m, v)
+#         s = torch.exp(self.z_prior_m)
+#         P_x_given_z = torch.distributions.Normal(m, s)
+#         log_P_x_given_z = P_x_given_z.log_prob(z)
+
+#         rec = log_P_x_given_z.sum(dim=1)
+       
+#         kl = ut.kl_normal(m, v, self.z_prior_m, self.z_prior_v)
+#         nelbo = kl + rec
 
         return nelbo, kl, rec
 
