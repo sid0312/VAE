bddb76046716c144fd7b7c941c3e92e8a3a2edd3
Siddhant Baldota <siddhantchemistry@gmail.com>
2022-11-02 19:49:15 -0700

    Update vae.py

--- src/codebase/models/vae.py
+++ src/codebase/models/vae.py
@@ -52,6 +52,8 @@ class VAE(nn.Module):
         x_hat = self.dec.decode(z)
         rec = torch.distributions.Normal(x_hat, self.z_prior_v).log_prob(x)
         kl = ut.kl_normal(m, v, self.z_prior_m, self.z_prior_v)
+        print(kl.size())
+        print(rec.size())
         nelbo = kl + rec
         
         
